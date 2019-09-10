80eb740a64503ae42aea64176d5dad4312dc0c51
itsmaisn <itsmaisn@gmail.com>
2019-09-09 18:56:14 -0700

    Cleaned up some code.

--- M01/Assignment/app/src/main/java/com/lambdaschool/androidinheritanceshopping/ui/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/lambdaschool/androidinheritanceshopping/ui/MainActivity.kt
@@ -14,6 +14,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val list = ArrayList<ShoppingItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,147 +23,125 @@ class MainActivity : AppCompatActivity() {
         rv_list.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
-            adapter = RecyclerViewAdapter(
-                shoppingItems = ArrayList(ShoppingList())
-            )
+            adapter = RecyclerViewAdapter(list)
         }
+
+        generateItems()
     }
 
     // Write a method in your main activity to generate test values and add them to a List
-    fun ShoppingList(): ArrayList<ShoppingItem> {
-
-        val shoppingItemList = ArrayList<ShoppingItem>()
-        val clothingItem = ArrayList<ClothingItem>()
-        val electronicItem = ArrayList<ElectronicItem>()
-        val musicItem = ArrayList<MusicItem>()
-
-        clothingItem.add(
-            ClothingItem(
-                R.color.colorClothingItem,
-                "Henley Top",
-                "Urban Outfitters",
-                "X-Small"
-            )
-        )
-        clothingItem.add(
-            ClothingItem(
-                R.color.colorClothingItem,
-                "Quilt Hoody",
-                "Patagonia",
-                "Small"
-            )
-        )
-        clothingItem.add(
-            ClothingItem(
-                R.color.colorClothingItem,
-                "Yoga Pants",
-                "lululemon",
-                "4"
-            )
-        )
-        clothingItem.add(
-            ClothingItem(
-                R.color.colorClothingItem,
-                "Wrap",
-                "Arc'teryx",
-                "X-Small"
-            )
-        )
-        clothingItem.add(
-            ClothingItem(
-                R.color.colorClothingItem,
-                "Hiking Pants",
-                "REI",
-                "X-Small"
-            )
-        )
-
-        electronicItem.add(
-            ElectronicItem(
-                R.color.colorElectronicItem,
-                "Noice Cancelling Headphones",
-                "Sony",
-                349.99
-            )
-        )
-        electronicItem.add(
-            ElectronicItem(
-                R.color.colorElectronicItem,
-                "Acer Monitor",
-                "Amazon",
-                89.99
-            )
-        )
-        electronicItem.add(
-            ElectronicItem(
-                R.color.colorElectronicItem,
-                "Nintendo Switch",
-                "BestBuy",
-                299.99
-            )
-        )
-        electronicItem.add(
-            ElectronicItem(
-                R.color.colorElectronicItem,
-                "iPad Pro",
-                "Apple",
-                999.00
-            )
-        )
-        electronicItem.add(
-            ElectronicItem(
-                R.color.colorElectronicItem,
-                "Google Pixel 3 XL",
-                "Google",
-                899.00
-            )
-        )
-
-        musicItem.add(
-            MusicItem(
-                R.color.colorMusicItem,
-                "Crosley Turntable",
-                "Amazon",
-                "No"
-            )
-        )
-        musicItem.add(
-            MusicItem(
-                R.color.colorMusicItem,
-                "Lana Del Rey: Born to Die",
-                "Target",
-                "No"
-            )
-        )
-        musicItem.add(
-            MusicItem(
-                R.color.colorMusicItem,
-                "Wicked: A New Musical",
-                "Barnes and Noble",
-                "Yes"
-            )
-        )
-        musicItem.add(
-            MusicItem(
-                R.color.colorMusicItem,
-                "Lana Del Rey: NFR",
-                "Urban Outfitters",
-                "Yes"
-            )
-        )
-        musicItem.add(
-            MusicItem(
-                R.color.colorMusicItem,
-                "Illenium: Ascend",
-                "AstralWerks Record",
-                "No"
-            )
-        )
-
-        shoppingItemList.addAll(clothingItem)
-        shoppingItemList.addAll(electronicItem)
-        shoppingItemList.addAll(musicItem)
-
-        return shoppingItemList
+    fun generateItems() {
+        sampleItems.forEach {
+            list.add(it)
+        }
     }
+
+    val sampleItems: ArrayList<ShoppingItem> = arrayListOf(
+        ClothingItem(
+            R.color.colorClothingItem,
+            "Henley Top",
+            "Urban Outfitters",
+            "X-Small"
+        ),
+
+        ClothingItem(
+            R.color.colorClothingItem,
+            "Quilt Hoody",
+            "Patagonia",
+            "Small"
+        ),
+
+        ClothingItem(
+            R.color.colorClothingItem,
+            "Yoga Pants",
+            "lululemon",
+            "4"
+        ),
+
+        ClothingItem(
+            R.color.colorClothingItem,
+            "Wrap",
+            "Arc'teryx",
+            "X-Small"
+        ),
+
+        ClothingItem(
+            R.color.colorClothingItem,
+            "Hiking Pants",
+            "REI",
+            "X-Small"
+        ),
+
+
+        ElectronicItem(
+            R.color.colorElectronicItem,
+            "Noice Cancelling Headphones",
+            "Sony",
+            349.99
+        ),
+
+        ElectronicItem(
+            R.color.colorElectronicItem,
+            "Acer Monitor",
+            "Amazon",
+            89.99
+        ),
+
+        ElectronicItem(
+            R.color.colorElectronicItem,
+            "Nintendo Switch",
+            "BestBuy",
+            299.99
+        ),
+
+        ElectronicItem(
+            R.color.colorElectronicItem,
+            "iPad Pro",
+            "Apple",
+            999.00
+        ),
+
+        ElectronicItem(
+            R.color.colorElectronicItem,
+            "Google Pixel 3 XL",
+            "Google",
+            899.00
+        ),
+
+
+        MusicItem(
+            R.color.colorMusicItem,
+            "Crosley Turntable",
+            "Amazon",
+            "No"
+        ),
+
+        MusicItem(
+            R.color.colorMusicItem,
+            "Lana Del Rey: Born to Die",
+            "Target",
+            "No"
+        ),
+
+        MusicItem(
+            R.color.colorMusicItem,
+            "Wicked: A New Musical",
+            "Barnes and Noble",
+            "Yes"
+        ),
+
+        MusicItem(
+            R.color.colorMusicItem,
+            "Lana Del Rey: NFR",
+            "Urban Outfitters",
+            "Yes"
+        ),
+
+        MusicItem(
+            R.color.colorMusicItem,
+            "Illenium: Ascend",
+            "AstralWerks Record",
+            "No"
+        )
+    )
 }
